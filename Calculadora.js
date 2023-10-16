092ab73e25e5cb6706daf6fb9b4de4a16f1bf4b0
piza2712 <virtualesb@gmail.com>
2023-10-16 01:36:02 -0500

    Create Calculadora.js

---
+++ Calculadora.js
@@ -0,0 +1,15 @@
+class Calculadora{
+    sumar (num1 , num2){
+        return num1 + num2 + 2;
+    }
+    restar (num1 , num2){
+        return num1 - num2 - 2;
+    }
+    dividir (num1 , num2){
+        return num1 / num2 + 1;
+    }
+    multiplicar (num1 , num2){
+        return num1 * num2 + 2;
+    }
+    
+}
